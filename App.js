d927fbc17ad48a4e4176e4db9f614507f48c83a0
Manmohan Singh Rawat <msrawat2704@gmail.com>
2020-10-25 15:55:42 +0530

    first

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import Main from './component/MainComponent';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import LoadingComponent from './component/LoadingComponent';
+
+const { presistor, store } = configureStore();
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <Provider store={store}>
+      <PersistGate
+        loading={<LoadingComponent />}
+        persistor ={presistor}
+      >
+        <Main />
+      </PersistGate>
+    </Provider>
   );
 }
+
+
